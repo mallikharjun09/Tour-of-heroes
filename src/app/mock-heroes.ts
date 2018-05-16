118ecc890515c6452ba7ac1b2444811a97b7ab41
mallikharjun09 <mallikharjun.kinthada@gmail.com>
2018-05-16 11:14:10 +0530

    Angular Project
    It is an Example for Angular project using VSC

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero'
+
+export const HEROES : Hero[]=[
+    {id : 1, name : 'Mallik'},
+    {id : 2, name : 'Hazarath'},
+    {id : 3, name : 'Govind'},
+    {id : 4, name : 'Nagul'},
+    {id : 5, name : 'Srinivas'},
+    {id : 6, name : 'Uday'}
+]
